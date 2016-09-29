38b41e6cdd90209287c73e01f9815b0901d724b5
Eder Leão Fernandes <ederleaofernandes@gmail.com>
2016-09-29 18:52:41 +0100

    Add horse display

--- src/horse.c
+++ src/horse.c
@@ -13,10 +13,26 @@ display_help_message(void)
     printf("\n");
 }
 
+static void 
+display_horse(void)
+{
+    printf("     >>\\.\n");
+    printf("    /_  )`.\n");
+    printf("   /  _)`^)`.   _.---. _\n");
+    printf("  (_,' \\  `^-)""      `.\\\n");
+    printf("        |              | \\\n");
+    printf("        \\              / |\n");
+    printf("       / \\  /.___.'\\  (\\ (_\n");
+    printf("      < ,\"||     \\ |`. \\`-'\n");
+    printf("       \\\\ ()      )|  )/\n");
+    printf("        |_>|>     /_] //\n");
+    printf("         /_]  hjw    /_]\n");
+}
+
 int 
 main(int argc, char *argv[]){
     int c;
-
+    display_horse();
     while ((c = getopt (argc, argv, "h")) != -1){
         switch (c){
             case 'h':{
