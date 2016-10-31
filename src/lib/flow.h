e69af49c554d3be4027faf502630add4a9a0e02c
Eder Leão Fernandes <ederleaofernandes@gmail.com>
2016-10-31 19:59:13 +0000

    Add license file and headers

--- src/lib/flow.h
+++ src/lib/flow.h
@@ -1,3 +1,14 @@
+/* Copyright (c) 2016-2017, Eder Leao Fernandes
+ * All rights reserved.
+ *
+ * The contents of this file are subject to the license defined in
+ * file 'doc/LICENSE'.
+ *
+ *
+ * Author: Eder Leao Fernandes <e.leao@qmul.ac.uk>
+ */
+
+
 #ifndef FLOW_H
 #define FLOW_H 1
 
