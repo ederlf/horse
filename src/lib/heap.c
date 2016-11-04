d6b7e334b9fd443b7280dad8126cafbf1c7e530b
Eder Leão Fernandes <ederleaofernandes@gmail.com>
2016-11-04 11:03:55 +0000

    Fix bug in heap delete

--- src/lib/heap.c
+++ src/lib/heap.c
@@ -42,7 +42,7 @@ min_heapify(struct heap *h, size_t idx, size_t size)
 {
     size_t left, right, min; 
     struct heap_node **arr = h->array;
-    do {
+    while (min != idx) {
         min = idx;
         left = heap_left_child(idx);
         right = heap_right_child(idx);
@@ -58,9 +58,10 @@ min_heapify(struct heap *h, size_t idx, size_t size)
 
         if(min != idx) {
             heap_swap(h, idx, min);
-        }
-        
-    }while (min != idx);
+            idx = min;
+            min = -1;
+        }   
+    }
 }
 
 struct heap_node* 
@@ -81,31 +82,32 @@ heap_delete(struct heap *h)
     return removed;
 }
 
-// static
-// void heap_display(struct heap *h) {
-//     size_t i;
-//     for(i=1; i <= h->size; ++i) {
-//         printf("|%ld|", h->array[i]->priority);
-//     }
-//     printf("\n");
-// }
+static
+void heap_display(struct heap *h) {
+    size_t i;
+    for(i=1; i <= h->size; ++i) {
+        printf("|%ld|", h->array[i]->priority);
+    }
+    printf("\n");
+}
 
-// int main(int argc, char const *argv[])
-// {
-//     struct heap *h = xmalloc(sizeof(struct heap));
-//     int i;
-//     for (i = 0; i < 100000000; ++i){
-//         struct heap_node *node = xmalloc(sizeof(struct heap_node));
-//         heap_insert(h, node, i);
-//     }
-//     // heap_display(h);
-//     size_t s = h->size;
-//     for (i = 1; i < s; ++i){
-//         free(heap_delete(h));
-//     }
-//     printf("%lu\n", h->size);
-//     heap_display(h);
-//     // heap_delete(h);
-//     // heap_display(h);
-//     return 0;
-// }
+int main(int argc, char const *argv[])
+{
+    struct heap *h = xmalloc(sizeof(struct heap));
+    int i;
+    for (i = 1; i < 10; ++i){
+        struct heap_node *node = xmalloc(sizeof(struct heap_node));
+        heap_insert(h, node, i);
+    }
+    // heap_display(h);
+    size_t s = h->size;
+    for (i = 1; i < s; ++i){
+        free(heap_delete(h));
+        heap_display(h);
+    }
+    printf("%lu\n", h->size);
+    
+    // heap_delete(h);
+    // heap_display(h);
+    return 0;
+}
