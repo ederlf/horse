6914c463056958f40979da7d3910d5d7ad56f252
ederlf <ederleaofernandes@gmail.com>
2016-12-29 15:37:19 +0000

    Allocate events in the heap

--- tests/scheduler_test.c
+++ tests/scheduler_test.c
@@ -3,6 +3,8 @@
 #include <time.h>
 #include "cmockery_horse.h"
 
+#define MAX_EVENTS_TEST 100000
+
 int cmpfunc (const void * a, const void * b)
 {
    return ( *(uint64_t*)a - *(uint64_t*)b );
@@ -31,21 +33,23 @@ void add_flow_event(void **state){
 void random_add_flow_event(void **state){
     struct scheduler* sch = scheduler_new();
     struct event ev_ret;
-    struct event ev[100000];
-    uint64_t times[100000];
+    struct event* ev = malloc(sizeof(struct event) * MAX_EVENTS_TEST);
+    uint64_t* times = malloc(sizeof(uint64_t) * MAX_EVENTS_TEST);
     int i;
     srand(time(NULL));
-    for (i = 0; i < 100000; ++i){
+    for (i = 0; i < MAX_EVENTS_TEST; ++i){
         times[i] =  rand(); 
-        init_event(&ev[i], EVENT_FLOW, times[i], i + 1);    
+        init_event(&ev[i], EVENT_FLOW, times[i], i + 1);   
         scheduler_insert(sch, &ev[i]);
     } 
-    qsort(times, 100000, sizeof(uint64_t), cmpfunc);
-    for (i = 0; i < 100000; ++i){
+    qsort(times, MAX_EVENTS_TEST, sizeof(uint64_t), cmpfunc);
+    for (i = 0; i < MAX_EVENTS_TEST; ++i){
         ev_ret = scheduler_dispatch(sch);
         assert_int_equal(ev_ret.time, times[i]);        
     } 
     scheduler_destroy(sch);
+    free(ev);
+    free(times);
 }
 
 void event_table(void **state){
@@ -59,10 +63,9 @@ void event_table(void **state){
     HASH_ADD(hh, table, id, sizeof(uint64_t), (struct event_hdr*) flow);
 
     HASH_FIND(hh, table, &id, sizeof(uint64_t), p);
-
     if (p){
         struct event_flow *f = (struct event_flow*)p;
-        printf("%lx\n", f->dpid);
+        assert_int_equal(0x9000000000000001, f->dpid);
     }
 
     HASH_ITER(hh, table, p, tmp) {
@@ -73,7 +76,8 @@ void event_table(void **state){
 
 int main(int argc, char* argv[]) {
     const UnitTest tests[] = {
-        // unit_test(add_flow_event),
+        unit_test(add_flow_event),
+        unit_test(random_add_flow_event),
         unit_test(event_table),
     };
     return run_tests(tests);
