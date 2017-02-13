fd03ac2505f7c5299967206ecb113b0174ce9348
ederlf <ederleaofernandes@gmail.com>
2017-02-13 17:58:07 +0000

    Add a new header with packet utils

---
+++ src/lib/packets.h
@@ -0,0 +1,40 @@
+#ifndef PACKET_H
+#define PACKET_H 1
+
+#define ETH_TYPE_IP            0x0800
+#define ETH_TYPE_IPV6          0x86dd
+#define ETH_TYPE_ARP           0x0806
+#define ETH_TYPE_VLAN          0x8100
+#define ETH_TYPE_VLAN_QinQ     0x88a8
+#define ETH_TYPE_MPLS          0x8847
+#define ETH_TYPE_MPLS_MCAST    0x8848
+
+
+#define VLAN_VID_MASK 0x0fff
+#define VLAN_VID_SHIFT 0
+#define VLAN_PCP_MASK 0xe000
+#define VLAN_PCP_SHIFT 13
+#define VLAN_PCP_BITMASK 0x0007 /* the least 3-bit is valid */
+#define VLAN_VID_MAX 4095
+#define VLAN_PCP_MAX 7
+
+struct vlan {
+    uint16_t ethertype;
+    uint16_t tag;
+};
+
+
+#define MPLS_TTL_MASK 0x000000ff
+#define MPLS_TTL_SHIFT 0
+#define MPLS_S_MASK 0x00000100
+#define MPLS_S_SHIFT 8
+#define MPLS_TC_MASK 0x00000e00
+#define MPLS_TC_SHIFT 9
+#define MPLS_LABEL_MASK 0xfffff000
+#define MPLS_LABEL_SHIFT 12
+
+struct mpls {
+    uint32_t fields; 
+};
+
+#endif
