2ef700af51ff4eda0dcf8b6a7f40bdb47cb54e10
ederlf <ederleaofernandes@gmail.com>
2017-04-18 16:17:01 +0100

    Modify test topology in Python

--- python/test.py
+++ python/test.py
@@ -1,14 +1,20 @@
 from horse import *
 
-sw1 = SDNSwitch(24)
-sw2 = SDNSwitch(42)
-sw3 = SDNSwitch(42)
+sw1 = SDNSwitch(42)
+# sw2 = SDNSwitch(42)
+# sw3 = SDNSwitch(42)
 
-sw1.add_port(1, "00:00:00:00:00:01")
-sw1.add_port(1, "00:00:00:00:00:02")
+h1 = Host()
+h2 = Host()
 
-sw2.add_port(1, "00:00:00:00:01:00")
-sw2.add_port(2, "00:00:00:00:02:00")
+sw1.add_port(1, "00:00:00:00:01:00")
+sw1.add_port(2, "00:00:00:00:02:00")
+
+h1.add_port(1, "00:00:00:00:00:01")
+h1.add_port(1, "00:00:00:00:00:02")
+
+# sw2.add_port(1, "00:00:00:00:01:00")
+# sw2.add_port(2, "00:00:00:00:02:00")
 
 # sw3.add_port(1, "00:00:00:01:00:00")
 # sw3.add_port(2, "00:00:00:02:00:00")
@@ -17,11 +23,17 @@ topo = Topology()
 
 # print topo.dp
 
-topo.add_switch(sw1)
-topo.add_switch(sw2)
-topo.add_switch(sw3)
+topo.add_node(sw1)
+topo.add_node(h1)
+topo.add_node(h2)
+# topo.add_node(sw2)
+# topo.add_node(sw3)
 
-topo.add_link(sw1, sw2, 2, 1)
+
+topo.add_link(sw1, h1, 1, 1)
+topo.add_link(sw1, h2, 2, 1)
+# topo.add_link(sw1, sw2, 2, 1)
+# topo.add_link(sw1, sw2, 2, 1)
 # topo.add_link(sw2, sw3, 2, 2)
 
 topo.start()
