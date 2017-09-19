8b0f2cad0f58a5c58cddb254d65f7525fee1e795
ederlf <ederleaofernandes@gmail.com>
2017-09-19 16:29:50 +0000

    Add hton64 and ntoh64

--- src/lib/util.h
+++ src/lib/util.h
@@ -15,6 +15,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <inttypes.h>
+#include <arpa/inet.h>
 
 #define UNUSED(x) (void)x;
 
@@ -26,5 +27,23 @@ uint32_t random_at_most(uint32_t max);
 char* file_to_string(const char * file_name, size_t *size);
 int nlz(uint32_t x);
 int ntz(uint32_t x);
+
+static inline uint64_t
+hton64(uint64_t n) {
+#if __BYTE_ORDER == __BIG_ENDIAN
+    return n;
+#else
+    return (((uint64_t)htonl(n)) << 32) + htonl(n >> 32);
+#endif
+}
+
+static inline uint64_t
+ntoh64(uint64_t n) {
+#if __BYTE_ORDER == __BIG_ENDIAN
+    return n;
+#else
+    return (((uint64_t)ntohl(n)) << 32) + ntohl(n >> 32);
+#endif
+}
 
 #endif
