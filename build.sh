20b1349e55dddd07056edf1b05e1c5a0fab0fb7a
ederlf <ederleaofernandes@gmail.com>
2018-03-03 12:36:17 +0000

    Update build

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ if [ -n "$TRAVIS" ]; then
 	sudo pip install cython
 	sudo apt-get install autoconf libtool build-essential pkg-config
 	sudo apt-get install libevent-dev
-	git clone https://github.com/ederlf/libcfluid_base.git && git checkout multi-client && 
-	cd $TRAVIS_BUILD_DIR/libcfluid_base && ./autogen.sh && ./configure && make
+	git clone https://github.com/ederlf/libcfluid_base.git && cd $TRAVIS_BUILD_DIR/libcfluid_base && 
+	git checkout multi-client && ./autogen.sh && ./configure && make
 	sudo make install
 	cd $TRAVIS_BUILD_DIR
 	./boot.sh && ./configure && make
