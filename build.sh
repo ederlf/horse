eea41a388d59915e429d4de3c2c23527210acc48
ederlf <ederleaofernandes@gmail.com>
2018-03-03 11:37:30 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,9 +7,7 @@ if [ -n "$TRAVIS" ]; then
 	sudo apt-get install autoconf libtool build-essential pkg-config
 	sudo apt-get install libevent-dev
 	git clone https://github.com/ederlf/libcfluid_base.git
-	cd libcfluid_base
-	./autogen && ./configure
-	make
+	cd libcfluid_base && ./autogen && ./configure && make
 	sudo make install
 	cd ..
 fi
