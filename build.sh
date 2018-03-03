9d29174d4835b460309706af3b7e596cb5d468df
ederlf <ederleaofernandes@gmail.com>
2018-03-03 12:28:10 +0000

    Fix dummy error in build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
-#!/bin/bash
+!/bin/bash
 
-#install deps if we are in travis
+install deps if we are in travis
 if [ -n "$TRAVIS" ]; then
 	sudo apt-get install python-pip
 	sudo pip install cython
 	sudo apt-get install autoconf libtool build-essential pkg-config
 	sudo apt-get install libevent-dev
 	git clone https://github.com/ederlf/libcfluid_base.git && cd $TRAVIS_BUILD_DIR/libcfluid_base
-	./autogen && ./configure && make
+	./autogen.sh && ./configure && make
 	sudo make install
 	cd $TRAVIS_BUILD_DIR
+	./boot.sh && ./configure && make
+	cd $TRAVIS_BUILD_DIR/python && python setup.py build_ext --inplace
+else
+	./boot.sh && ./configure && make
+	cd python && python setup.py build_ext --inplace
 fi
-
-./boot.sh && ./configure && make
-cd $TRAVIS_BUILD_DIR/python && python setup.py build_ext --inplace
-
