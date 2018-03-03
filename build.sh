b15a6c69a5449cccfb4c027e11698f1e80551b94
ederlf <ederleaofernandes@gmail.com>
2018-03-03 11:53:19 +0000

    Trying to fix travis build

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 #install deps if we are in travis
 if [ -n "$TRAVIS" ]; then
 	sudo apt-get install pip
-	pip install cython
+	sudo pip install cython
 	sudo apt-get install autoconf libtool build-essential pkg-config
 	sudo apt-get install libevent-dev
-	git clone https://github.com/ederlf/libcfluid_base.git
-	cd $TRAVIS_BUILD_DIR/libcfluid_base && ./autogen && ./configure && make
+	git clone https://github.com/ederlf/libcfluid_base.git && cd $TRAVIS_BUILD_DIR/libcfluid_base
+	./autogen && ./configure && make
 	sudo make install
 fi
 
 ./boot.sh && ./configure && make
-cd $TRAVIS_BUILD_DIR/horse/python && python setup.py build_ext --inplace
+cd $TRAVIS_BUILD_DIR/python && python setup.py build_ext --inplace
 
