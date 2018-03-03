89ada59c202fb1a6c16e77b0a3bed0dedc547ef1
ederlf <ederleaofernandes@gmail.com>
2018-03-03 11:43:33 +0000

    Update travis build files

--- build.sh
+++ build.sh
@@ -7,11 +7,10 @@ if [ -n "$TRAVIS" ]; then
 	sudo apt-get install autoconf libtool build-essential pkg-config
 	sudo apt-get install libevent-dev
 	git clone https://github.com/ederlf/libcfluid_base.git
-	cd libcfluid_base && ./autogen && ./configure && make
+	cd $TRAVIS_BUILD_DIR/libcfluid_base && ./autogen && ./configure && make
 	sudo make install
-	cd ..
 fi
 
 ./boot.sh && ./configure && make
-cd python && python setup.py build_ext --inplace
+cd $TRAVIS_BUILD_DIR/horse/python && python setup.py build_ext --inplace
 
