6d404ca9bcfb603642d884293755349a410c07a1
ederlf <ederleaofernandes@gmail.com>
2018-04-05 15:45:30 +0100

    Some ugly workaround to get the router binding interface

--- src/net/routing/bgp.c
+++ src/net/routing/bgp.c
@@ -2,10 +2,9 @@
 #include "lib/util.h"
 #include <netemu/netns.h>
 
-
-
 static void bgp_start(struct routing *rt, char *rname);
 static void bgp_advertise(struct routing *rt);
+static char* find_router_id(struct bgp* p);
 
 void 
 bgp_init(struct bgp *p, char *config_file)
@@ -22,9 +21,15 @@ static void
 bgp_start(struct routing *rt, char * rname)
 {
     struct bgp *p = (struct bgp*) rt;
-    netns_run(rname, "env exabgp.daemon.daemonize=true " 
+    char *router_id = find_router_id(p);
+    printf("Router id %s\n", router_id);
+    if (router_id != NULL) {
+        netns_run(rname, "env exabgp.daemon.daemonize=true "
+              "exabgp.tcp.bind=%s " 
               "exabgp.log.destination=syslog exabgp %s",
-              p->config_file); 
+              router_id, p->config_file); 
+    }
+    free(router_id);
 }
 
 static void 
@@ -32,4 +37,30 @@ bgp_advertise(struct routing *rt)
 {
     struct bgp *p = (struct bgp*) rt;
     UNUSED(p);   
+}
+
+static char* 
+find_router_id(struct bgp* p)
+{
+    FILE *cfile;
+    char *tmp, *ip;
+    char line[100];
+    cfile = fopen(p->config_file, "r");
+    ip = NULL;
+    while( fgets(line, sizeof(line), cfile) != NULL )  {
+        tmp =  strstr (line,"router-id");
+        if (tmp != NULL ){
+            ip = xmalloc(20);
+            tmp += sizeof("router_id");
+            int i = 0;
+            while(*tmp != ';'){
+                ip[i] = *tmp;
+                i++;
+                tmp ++;
+            }
+            ip[i] = '\0';
+        }   
+    }
+    fclose(cfile);
+    return ip;
 }
