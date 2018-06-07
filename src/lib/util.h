aa52887d7a066df9ba98150dc25c26ed4f8de28a
ederlf <ederleaofernandes@gmail.com>
2018-06-07 10:51:06 +0100

    Inline crc32

--- src/lib/util.h
+++ src/lib/util.h
@@ -25,7 +25,7 @@ void *xmalloc(size_t size);
 void *xrealloc(void *v, int size);
 uint32_t random_at_most(uint32_t max);
 char* file_to_string(const char * file_name, size_t *size);
-void crc32(const void *data, size_t n_bytes, uint32_t *crc);
+// void crc32(const void *data, size_t n_bytes, uint32_t *crc);
 int nlz(uint32_t x);
 int ntz(uint32_t x);
 uint8_t get_ip_family(char *ip);
@@ -49,6 +49,29 @@ ntoh64(uint64_t n) {
 #else
     return (((uint64_t)ntohl(n)) << 32) + ntohl(n >> 32);
 #endif
+}
+
+/* Simple public domain implementation of the standard CRC32 checksum.*/
+static inline uint32_t 
+crc32_for_byte(uint32_t r) {
+  int j;
+  for(j = 0; j < 8; ++j)
+    r = (r & 1? 0: (uint32_t)0xEDB88320L) ^ r >> 1;
+  return r ^ (uint32_t)0xFF000000L;
+}
+
+static inline void 
+crc32(const void *data, size_t n_bytes, uint32_t *crc) {
+    static uint32_t table[0x100];
+    size_t i;
+    if(!*table) {
+        for(i = 0; i < 0x100; ++i) {
+            table[i] = crc32_for_byte(i);
+        }
+    }
+    for(i = 0; i < n_bytes; ++i){
+        *crc = table[(uint8_t)*crc ^ ((uint8_t*)data)[i]] ^ *crc >> 8;
+    }
 }
 
 #endif
